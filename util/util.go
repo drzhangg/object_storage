2239454eca45c42d06cbcfc52a8027ffe8477d34
drzhang <654014730@qq.com>
2020-09-03 22:02:33 +0800

    add update sha

--- util/util.go
+++ util/util.go
@@ -1,9 +1,17 @@
 package util
 
-import "hash"
+import (
+	"crypto/sha1"
+	"hash"
+)
 
 type Sha1Stream struct {
 	_sha1 hash.Hash
 }
 
-
+func (obj *Sha1Stream) Update(data []byte) {
+	if obj._sha1 == nil {
+		obj._sha1 = sha1.New()
+	}
+	obj._sha1.Write(data)
+}
